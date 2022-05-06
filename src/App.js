c002cb12819331f5fd096f5745bedf72b5bb7e81
setykarimi <setykarimi80@gmail.com>
2022-05-06 22:28:26 +0430

    add layout to app js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import {Route , Routes , Link} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AboutUs from './pages/AboutUs'
+import Layout from './Layout/Layout'
+import routes from './routes'
+import NotFound from './pages/NotFound'
 
-function App () {
-return (
-  <>
-  <header>
-    <nav>
-    <ul>
-      <li>
-      <Link to="/">Home</Link>
-      </li>
-      <li>
-      <Link to="/about-us">About Us</Link>
-            </li>
-    </ul>
-      
-    </nav>
-  </header>
-  <Routes>
-    <Route path='/' exact={true} element={<HomePage/>}/>
-    <Route path='/about-us' element={<AboutUs/>}/>
-  </Routes>
-  </>
-)
+function App() {
+  return (
+    <>
+
+<Layout>
+      <Routes>
+        <Route path='/' exact={true} element={<HomePage />} />
+        <Route path='/about-us' element={<AboutUs />} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+      </Layout>
+    </>
+  )
 }
 
 export default App
