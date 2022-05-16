f96cbf1bf65ef830c0c36b5ee43c5eea4bc2f764
Sety <setykarimi80@gmail.com>
2022-05-16 13:03:19 +0430

    add amid , moein , moataradef , suggestions results in diffrent variables and define an object for data bases , add map function for each database result

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,15 +5,17 @@ const HomePage = () => {
   const [input, setInput] = useState('');
   const [meaning, setMeaning] = useState([])
   const [database, setDatabase] = useState('dehkhoda');
-  const [suggestions, setSuggestions] = useState('آسمان');
-  const databases = { dehkhoda : "dehkhoda" , amid: "amid"} 
-  console.log(databases); 
-  console.log(databases.dehkhoda);
-  console.log(databases.amid);
+  const [suggestions, setSuggestions] = useState([]);
+  const databases =
+  { dehkhoda : "dehkhoda" , amid: "amid" , moein : "moein" , motaradef : "motaradef" , isfahani : "isfahani"} ;
+  const [amidDatabase, setAmidDatabase] = useState([])
+  const [moeinDatabase, setMoeinDatabase] = useState([])
+  const [motaradefDatabase, setMotaradefDatabase] = useState([])
+  const [isfahaniDatabase, setIsfahaniDatabase] = useState([])
 
   const token = "68283.WF5AdpjT2PSP12ePMldnNyuByZJ6kdGXsuerEhjd"
 
-  const setSuggestionHandler = () => {
+  const setMeaningHandler = () => {
     axios
       .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${database}`)
       .then((res) => {
@@ -32,32 +34,79 @@ const HomePage = () => {
       .catch((err) => console.log(err))
   }, [database])
 
-
-  const changeInputHandler = (e) => {
-    setInput(e.target.value)
+  const amidDatabaseHandler = () => {
     axios
-      .get(`http://api.vajehyab.com/v3/suggest?token=${token}&q=${input}`)
+      .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.amid}`)
       .then((res) => {
-        setSuggestions(res.data.data.suggestion)
+        console.log(res.data.data.results)
+        setAmidDatabase(res.data.data.results)
       })
       .catch((err) => console.log(err))
   }
 
+  const moeinDatabaseHandler = () => {
+    axios
+      .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.moein}`)
+      .then((res) => {
+        console.log(res.data.data.results)
+        setMoeinDatabase(res.data.data.results)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  const motaradefDatabaseHandler = () => {
+    axios
+      .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.motaradef}`)
+      .then((res) => {
+        console.log(res.data.data.results)
+        setMotaradefDatabase(res.data.data.results)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  const suggestionsHandler = () => {
+   axios
+   .get (`http://api.vajehyab.com/v3/suggest?token=${token}&q=${input}`)
+   .then ((res) => {
+     console.log("suggestion" , res.data.data.suggestion)
+     setSuggestions(res.data.data.suggestion)
+   })
+   .catch((err) => {})
+  }
+
+  const isfahaniDatabaseHandler = () => {
+    axios
+    .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.isfahani}`)
+    .then((res) => {
+      console.log(res.data.data.results)
+      setIsfahaniDatabase(res.data.data.results)
+    })
+    .catch((err) => console.log(err))
+  }
+
+
+  const changeInputHandler = (e) => {
+    setInput(e.target.value)
+    
+  }
+
   const changDatabaseHandler = (e) => {
-    setDatabase(e.target.value)
-    setSuggestionHandler()
+    isfahaniDatabaseHandler()
   }
 
   const getResultHandler = () => {
-    setSuggestionHandler()
+    setMeaningHandler()
+    amidDatabaseHandler()
+    moeinDatabaseHandler()
+    motaradefDatabaseHandler()
+    suggestionsHandler()
   }
 
   return (
     <div className="App">
 
       <div>
-        <button onClick={changDatabaseHandler} value="dehkhoda">دهخدا</button>
-        <button onClick={changDatabaseHandler} value="moein">معین</button>
+        <button onClick={changDatabaseHandler} value="isfahani">لهجه و گویش اصفهانی</button>
 
       </div>
 
@@ -65,14 +114,63 @@ const HomePage = () => {
 
 
       <button onClick={getResultHandler}>search</button>
-      <span>
+
+     
+
         {meaning.map((item, key) => {
           return (
             <span key={key}>
+               <h4>{item.source}</h4>
               {item.text}
             </span>
           )
-        })}</span>
+        })}
+
+
+        {amidDatabase.map((item, key) => {
+          return (
+            <div key={key}>
+              <h4> {item.source}</h4><span>تلفظ {item.pron}</span>
+              <p>{item.text}</p>
+            </div>
+           )
+        })}
+     
+
+    
+        {moeinDatabase.map((item, key) => {
+          return (
+            <div key={key}>
+              <h4> {item.source}</h4><span></span>
+              <p><b>معنی :</b> {item.text}</p>
+            </div>
+           )
+        })}
+     
+
+      
+        {motaradefDatabase.map((item, key) => {
+          return (
+            <div key={key}>
+              <h4> {item.source}</h4>
+              <p><b>معنی :</b> {item.text}</p>
+            </div>
+           )
+        })}
+     
+
+      {suggestions.map((suggest , key) => {
+       return (
+         <div key={key}>
+         <span>
+           {suggest}, 
+         </span>
+         </div>
+       )
+     })}
+     
+      
+        
     </div>
   );
 }
