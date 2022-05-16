d785c6c12d9bae52b097f574eb55a7a10ed643a1
Sety <setykarimi80@gmail.com>
2022-05-16 09:37:44 +0430

    add container class to {children}

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,11 +1,14 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import './layout.scss'
 
 const Layout = ({ children }) => {
     return (
         <>
             <Header />
+            <div className="container">
             {children}
+            </div>
             <Footer />
         </>
     )
