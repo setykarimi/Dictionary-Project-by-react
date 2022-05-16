3925e3bbf425ad360ca6b6496eed30382c6408b9
setykarimi <setykarimi80@gmail.com>
2022-05-17 01:33:42 +0430

    add hamburger menu and close button to component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,14 @@
 import {Link} from 'react-router-dom';
+import Menu from '../img/hamburger-menu.svg';
+import Close from '../img/close-icon.svg'
 
 const Navigation = () => {
  return (
     <header>
+    <button className='hamburger-menu'><img src={Menu}/></button>
     <nav>
+    <button className='close-icon'><img src={Close}/></button>
+
       <ul>
         <li>
           <Link to="/">خانه</Link>
@@ -11,9 +16,7 @@ const Navigation = () => {
         <li>
           <Link to="/about-us">درباره ما</Link>
         </li>
-
       </ul>
-
     </nav>
   </header>
  )
