2cc2d9b354ebb569e4f920ce4c96ffc03c3665e4
Sety <setykarimi80@gmail.com>
2022-05-17 17:55:13 +0430

    add function for close and show menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,27 @@
 import {Link} from 'react-router-dom';
 import Menu from '../img/hamburger-menu.svg';
 import Close from '../img/close-icon.svg'
+import { useState } from 'react';
 
 const Navigation = () => {
+
+  const [showMenu , setShowMenu] = useState(false)
+
+  const closeMenuHandler = () => {
+    setShowMenu(false)
+    console.log('close-menu')
+  }
+
+  const showMenuHandler = () => {
+    setShowMenu(true)
+    console.log('show')
+  }
+
  return (
     <header>
-    <button className='hamburger-menu'><img src={Menu}/></button>
-    <nav>
-    <button className='close-icon'><img src={Close}/></button>
+    <button className='hamburger-menu' onClick={showMenuHandler}><img src={Menu}/></button>
+    <nav className={showMenu == false ? 'closed' : ''}>
+    <button className='close-icon' onClick={closeMenuHandler}><img src={Close}/></button>
 
       <ul>
         <li>
