da57363845ae430d886f06c5c8605724f4ec3da8
setykarimi <setykarimi80@gmail.com>
2022-06-02 13:48:58 +0430

    remove setting menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,11 @@
 
-import Setting from '../../components/Header/Setting';
 import Logo from '../../components/Header/Logo';
 
 const Header = ({ children }) => {
     return (
         <div className="header">
             <Logo />
-                {children}
-                
-            <Setting />
+                {children}  
         </div>
     )
 }
