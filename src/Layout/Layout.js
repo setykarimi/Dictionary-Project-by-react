6f2711291cc75d1ac5cb33953b605bef89caa98a
setykarimi <setykarimi80@gmail.com>
2022-06-02 09:21:53 +0430

    remove header and footer and contaier class from layoout

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,14 +1,13 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+
 
 const Layout = ({ children }) => {
     return (
         <>
-            <Header />
-            <div className="container">
+            {/* <Header /> */}
+            <div>
             {children}
             </div>
-            <Footer />
+            {/* <Footer /> */}
         </>
     )
 }
