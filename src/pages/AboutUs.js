3071584122806ed75105ae3bc927b674f4191c9c
setykarimi <setykarimi80@gmail.com>
2022-06-02 09:22:49 +0430

    add header and sidebar to aboutus page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,14 @@
+import Header from "../components/Header/Header"
+import Sidebar from "../components/Sidebar/Sidebar"
 
 const AboutUs = () => {
     return (
-            <p>About Us</p>
+        <>
+            <Header />
+            <div className='container'>
+                <Sidebar />
+            </div>
+        </>
     )
 }
 
