aded8f8e5e7ed17d3bcadab45fc607677f3e48fa
setykarimi <setykarimi80@gmail.com>
2022-06-02 13:51:15 +0430

    add active class for current url, add hamburger menu for showing and hiding in respponsive view

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,33 +1,46 @@
-import {Link} from 'react-router-dom';
-import Home from '../../img/home.svg'
-import AboutUs from '../../img/about-us.svg'
-import './sidebar.scss'
+import {useState} from 'react';
+import { Link } from 'react-router-dom';
+import Home from '../../img/home.svg';
+import AboutUs from '../../img/about-us.svg';
+import './sidebar.scss';
+import HamburgerMenu from './HamburgerMenu';
+import { useLocation } from "react-router-dom"
+
 
 const Sidebar = () => {
 
+  const sampleLocation = useLocation();
+const currentUrl = sampleLocation.pathname;
+  const [showSidebar , setShowSidebar] = useState(true);
+
+  
+  
   
 
- return (
+
+
+  return (
     <>
-   <div className='sidebar'>
-      <ul>
-        <li >
-          <Link to="/" className='sidebar__active-menu'>
-          <img src={Home} alt='home'/>
-          <span>خانه</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/about-us" >
-          <img src={AboutUs} alt="about-us"/>
-          <span>درباره ما</span>
-          </Link>
-        </li>
-      </ul>
+      <div className={'sidebar ' + (showSidebar ? 'activeSidebar' : '')}>
+        <ul>
+          <li >
+            <Link to="/" className={currentUrl == '/' ? 'sidebar__active-menu' : ''}>
+              <img src={Home} alt='home' />
+              <span>خانه</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/about-us"  className={currentUrl == '/about-us' ? 'sidebar__active-menu' : ''}>
+              <img src={AboutUs} alt="about-us" />
+              <span>درباره ما</span>
+            </Link>
+          </li>
+        </ul>
       </div>
-   
-  </>
- )
+      <HamburgerMenu onClickMenu={()=> setShowSidebar(!showSidebar)}/>
+
+    </>
+  )
 }
 
 export default Sidebar;
