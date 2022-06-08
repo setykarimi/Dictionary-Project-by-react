a224090c80584035789ff44ba76b1f3d6d37fee0
Sety <setykarimi80@gmail.com>
2022-06-08 16:50:03 +0430

    remove content

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,11 +3,7 @@ import { Link } from "react-router-dom"
 const NotFound = () => {
     return(
         <>
-            <p>404
-            <br>
-                the page not found
-            </br></p>
-            <Link to='/'>Go to Home page</Link>
+            
         </>
     )
 }
