a7f23e92255aa8393348f52ec6f9d3c1af655341
setykarimi <setykarimi80@gmail.com>
2022-06-17 18:30:44 +0430

    add social media for profile box

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,9 @@
 import Header from "../components/Header/Header"
 import Sidebar from "../components/Sidebar/Sidebar"
 import Image from '../img/sety.jpg'
+import Instagram from '../img/instagram.svg'
+import Linkedin from '../img/linkedin.svg'
+import Whatsapp from '../img/whatsapp.svg'
 import '../styles/about.scss'
 
 const AboutUs = () => {
@@ -14,7 +17,7 @@ const AboutUs = () => {
                     <div className="about-section">
                         <div>
                             <p> این وبسایت با استفاده از api که سایت واژه یاب از لغت نامه‌های مختلف در اختیار توسعه دهندگان وب قرار می‌دهد، راه‌اندازه شده‌است.
-                                <br></br> هدف این پروژه دسترسی هر چه سریعتر و راه‌تر به لغت نامه‌های زبان فارسی است تا پارسی زبانان بتوانند در کمترین سرعت معنی واژه مورد نظر خودشان را در برترین لغت‌نامه‌های فارسی بیابند.
+                                <br></br> هدف این پروژه دسترسی هر چه سریعتر و راحت‌تر به لغت نامه‌های زبان فارسی است تا پارسی زبانان بتوانند در کمترین سرعت معنی واژه مورد نظر خودشان را در برترین لغت‌نامه‌های فارسی بیابند.
                                 <br></br>
                                 من، ستایش کریمی سعی کردم تا با طراحی رابط کاربری ساده و کاربرپسند، وبسایتی در اختیار شما کاربران عزیز قرار دهم.
                                 در آپدیت بعدی پروژه تبدیل گفتار به متن نیز اضافه خواهد شد تا با استفاده از api مربوطه بتوان با کلام واژه مورد نظر خود را جستجو کرد. </p>
@@ -22,7 +25,22 @@ const AboutUs = () => {
                         <div>
                             <div className="box">
                                 <div className="box__profile">
-                                    <img src={Image}/>
+                                    <img src={Image} />
+                                </div>
+
+                                <div className="social-medias">
+                                    <a className="media" href="https://www.instagram.com/setykarimi/">
+                                        <div className="media__icon"><img src={Instagram}/></div>
+                                        <div className="media__address">setykarimi</div>
+                                    </a>
+                                    <a className="media" href="https://www.linkedin.com/in/setayesh-karimi-1a5669179/">
+                                        <div className="media__icon"><img src={Linkedin}/></div>
+                                        <div className="media__address">setykarimi</div>
+                                    </a>
+                                    <a className="media" href="https://wa.me/09190979722">
+                                        <div className="media__icon"><img src={Whatsapp}/></div>
+                                        <div className="media__address">+98 9190979722</div>
+                                    </a>
                                 </div>
                             </div>
                         </div>
