5462814c9cbce4e57838014b3f16cc8949864160
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-05 21:20:04 +0330

    remove then and catch

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -1,11 +1,9 @@
 import { http } from "./httpService";
 const token = "68283.WF5AdpjT2PSP12ePMldnNyuByZJ6kdGXsuerEhjd"
 
-export const SearchService = ({ input, database, setDatabase}) => {
+export const SearchService = ({ input, database}) => {
     return (
         http.get(`/search?token=${token}&q=${input}&type=exact&filter=${database}`)
-            .then((res) => ()=> setDatabase(res.data.data.results))
-            .catch((err) => console.log(err))
     )
 }
 
