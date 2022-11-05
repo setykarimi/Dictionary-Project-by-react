38b6e5c5e908c9171aa46800ac8c461311262fd5
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-05 21:19:43 +0330

    add SearchBar in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 
-const Header = ({ children }) => {
+import Search from '../../img/search-icon.svg';
+import Voice from '../../img/voice.svg';
+
+const Header = ({ input, changeInputHandler, getResultHandler}) => {
     return (
         <div className="header">
             <div className="header-logo">
                 <span>واژه یابـــــــــ</span>
             </div>
-            {children}
+            <div className='header-search_input'>
+                <img src={Search} alt="search-icon" />
+                <input placeholder='جستجو ...' value={input} type="text" onChange={changeInputHandler} onKeyDown={getResultHandler} />
+                <img src={Voice} alt="search-icon" />
+            </div>
         </div>
     )
 }
