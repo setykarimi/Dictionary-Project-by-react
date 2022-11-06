d609a5836ae9d1891544f0f7f8fe7b8e3fcf1285
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-06 20:18:05 +0330

    add location for showing search input

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,23 @@
 
 import Search from '../../img/search-icon.svg';
 import Voice from '../../img/voice.svg';
+import { useLocation } from 'react-router-dom';
 
-const Header = ({ input, changeInputHandler, getResultHandler}) => {
+const Header = ({ input, changeInputHandler, getResultHandler }) => {
+    const { pathname } = useLocation();
+    console.log(pathname);
     return (
         <div className="header">
             <div className="header-logo">
                 <span>واژه یابـــــــــ</span>
             </div>
-            <div className='header-search_input'>
-                <img src={Search} alt="search-icon" />
-                <input placeholder='جستجو ...' value={input} type="text" onChange={changeInputHandler} onKeyDown={getResultHandler} />
-                <img src={Voice} alt="search-icon" />
-            </div>
+            {pathname === "/" &&
+                <div className='header-search_input'>
+                    <img src={Search} alt="search-icon" />
+                    <input placeholder='جستجو ...' value={input} type="text" onChange={changeInputHandler} onKeyDown={getResultHandler} />
+                    <img src={Voice} alt="search-icon" />
+                </div>
+            }
         </div>
     )
 }
