6479358df00fea93be24674f0a3efda6fcc20233
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-07 21:41:14 +0330

    add new token and edit dehkhoda function, comment other handlers

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Search from '../img/search-icon.svg';
-import Voice from '../img/voice.svg';
 import '../components/Header/header.scss'
 import Header from '../components/Header/Header';
 import Sidebar from '../components/Sidebar/Sidebar';
 import DatabaseBox from '../components/Database/Database';
-import '../styles/home.scss'
+import '../styles/home.scss';
+import { SearchService } from '../services/searchService';
+import { http } from '../services/httpService';
 
 const HomePage = () => {
   const [input, setInput] = useState('');
@@ -14,106 +14,100 @@ const HomePage = () => {
   const [database, setDatabase] = useState('dehkhoda');
   const [suggestions, setSuggestions] = useState([]);
   const databases =
-    { dehkhoda: "dehkhoda", amid: "amid", moein: "moein", motaradef: "motaradef", isfahani: "isfahani" , en2fa : "en2fa" };
-  const [amidDatabase, setAmidDatabase] = useState([])
-  const [moeinDatabase, setMoeinDatabase] = useState([])
-  const [motaradefDatabase, setMotaradefDatabase] = useState([])
+    { dehkhoda: "dehkhoda", amid: "amid", moein: "moein", motaradef: "motaradef", isfahani: "isfahani", en2fa: "en2fa" };
+  const [amidDatabase, setAmidDatabase] = useState([]);
+  const [moeinDatabase, setMoeinDatabase] = useState([]);
+  const [motaradefDatabase, setMotaradefDatabase] = useState([]);
+  const [englishToPersian, setEnglishToPersian] = useState([]);
+  const [showDatabases, setShowDatabases] = useState(false);
 
-  const [englishToPersian, setEnglishToPersian] = useState([])
-  const [showDatabases, setShowDatabases] = useState(false)
+  const token = "68284.t5Gzego6SX28dzh71Un3aUZMAM2a0GIY7pkyzNEo";
 
-  const token = "68283.WF5AdpjT2PSP12ePMldnNyuByZJ6kdGXsuerEhjd"
+  const dehkhodaDatabaseHandler = () => {
 
-  const setMeaningHandler = () => {
-    axios
-      .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${database}`)
+    http
+      .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.dehkhoda}`)
       .then((res) => {
         setDehkhodaDatabase(res.data.data.results)
       })
       .catch((err) => console.log(err))
   }
 
+
   useEffect(() => {
     axios
       .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${database}`)
       .then((res) => {
-        setMeaningHandler()
-        amidDatabaseHandler()
-        moeinDatabaseHandler()
-        motaradefDatabaseHandler()
-        suggestionsHandler()
-        showNewDatabases()
-        englishToPersianHandler()
+        dehkhodaDatabaseHandler()
+        // amidDatabaseHandler()
+        // moeinDatabaseHandler()
+        // motaradefDatabaseHandler()
+        // suggestionsHandler()
+        // showNewDatabases()
+        // englishToPersianHandler()
       })
       .catch((err) => console.log(err))
   }, [input])
 
-  const amidDatabaseHandler = () => {
-    axios
-      .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.amid}`)
-      .then((res) => {
-        console.log(res.data.data.results)
-        setAmidDatabase(res.data.data.results)
-      })
-      .catch((err) => console.log(err))
-  }
+  // const amidDatabaseHandler = () => {
+  //   axios
+  //     .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.amid}`)
+  //     .then((res) => {
+  //       setAmidDatabase(res.data.data.results)
+  //     })
+  //     .catch((err) => console.log(err))
+  // }
 
-  const moeinDatabaseHandler = () => {
-    axios
-      .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.moein}`)
-      .then((res) => {
-        console.log(res.data.data.results)
-        setMoeinDatabase(res.data.data.results)
-      })
-      .catch((err) => console.log(err))
-  }
+  // const moeinDatabaseHandler = () => {
+  //   axios
+  //     .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.moein}`)
+  //     .then((res) => {
+  //       setMoeinDatabase(res.data.data.results)
+  //     })
+  //     .catch((err) => console.log(err))
+  // }
 
-  const motaradefDatabaseHandler = () => {
-    axios
-      .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.motaradef}`)
-      .then((res) => {
-        console.log(res.data.data.results)
-        setMotaradefDatabase(res.data.data.results)
-      })
-      .catch((err) => console.log(err))
-  }
+  // const motaradefDatabaseHandler = () => {
+  //   axios
+  //     .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.motaradef}`)
+  //     .then((res) => {
+  //       setMotaradefDatabase(res.data.data.results)
+  //     })
+  //     .catch((err) => console.log(err))
+  // }
 
-  const englishToPersianHandler = () => {
-    axios
-      .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.en2fa}`)
-      .then((res) => {
-        console.log(res.data.data.results)
-        setEnglishToPersian(res.data.data.results)
-      })
-      .catch((err) => console.log(err))
-  }
+  // const englishToPersianHandler = () => {
+  //   axios
+  //     .get(`http://api.vajehyab.com/v3/search?token=${token}&q=${input}&type=exact&filter=${databases.en2fa}`)
+  //     .then((res) => {
+  //       setEnglishToPersian(res.data.data.results)
+  //     })
+  //     .catch((err) => console.log(err))
+  // }
 
-  const suggestionsHandler = () => {
-    axios
-      .get(`http://api.vajehyab.com/v3/suggest?token=${token}&q=${input}`)
-      .then((res) => {
-        console.log("suggestion", res.data.data.suggestion)
-        setSuggestions(res.data.data.suggestion)
-      })
-      .catch((err) => { })
-  }
-
+  // const suggestionsHandler = () => {
+  //   axios
+  //     .get(`http://api.vajehyab.com/v3/suggest?token=${token}&q=${input}`)
+  //     .then((res) => {
+  //       setSuggestions(res.data.data.suggestion)
+  //     })
+  //     .catch((err) => { })
+  // }
 
   const changeInputHandler = (e) => {
     setInput(e.target.value)
   }
 
- 
 
   const getResultHandler = (e) => {
     if (e.key === 'Enter') {
-      setMeaningHandler()
-      amidDatabaseHandler()
-      moeinDatabaseHandler()
-      motaradefDatabaseHandler()
-      suggestionsHandler()
-      showNewDatabases()
-      englishToPersianHandler()
+      dehkhodaDatabaseHandler()
+      // amidDatabaseHandler()
+      // moeinDatabaseHandler()
+      // motaradefDatabaseHandler()
+      // suggestionsHandler()
+      // showNewDatabases()
+      // englishToPersianHandler()
     }
   }
 
@@ -122,35 +116,22 @@ const HomePage = () => {
   }
 
   const similarWordHandler = (e) => {
-    console.log(e.target.innerText);
     setInput(e.target.innerText)
-    setMeaningHandler()
-    amidDatabaseHandler()
-    moeinDatabaseHandler()
-    motaradefDatabaseHandler()
-    englishToPersianHandler()
+    dehkhodaDatabaseHandler()
+    // amidDatabaseHandler()
+    // moeinDatabaseHandler()
+    // motaradefDatabaseHandler()
+    // englishToPersianHandler()
     // suggestionsHandler()
   }
 
   return (
     <>
-
-      <Header>
-        <div className='header-search_input'>
-          <img src={Search} alt="search-icon" />
-          <input placeholder='جستجو ...' value={input} type="text" onChange={changeInputHandler} onKeyDown={getResultHandler} />
-          <img src={Voice} alt="search-icon" />
-        </div>
-      </Header>
-
+      <Header input={input} changeInputHandler={changeInputHandler} getResultHandler={getResultHandler} />
 
       <div className='container'>
         <Sidebar />
-
-
-
         <section className='content'>
-
           <div className='search-term'>
             <span>واژه جستجو شده: </span>
             <input placeholder='واژه مورد نظر خود را جستجو کنید' value={input} disabled />
