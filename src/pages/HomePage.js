5d72b2f6ef676469df43a3ba9f74c1a86794b1dd
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-11 17:26:00 +0330

    remove useless handlers

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,78 +1,8 @@
-import { useEffect, useState } from 'react';
 import '../components/Header/header.scss'
-import Header from '../components/Header/Header';
-import Sidebar from '../components/Sidebar/Sidebar';
 import DatabaseBox from '../components/Database/Database';
 import '../styles/home.scss';
-import { http } from '../services/httpService';
 
 const HomePage = ({ input, loading, resultData }) => {
-
-
-
-  // const motaradefDatabaseHandler = () => {
-  //   http
-  //     .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.motaradef}`)
-  //     .then((res) => {
-  //       console.log('motaradef' , res.data);
-  //       setResultData({...resultData, motaradef: res.data.data.results});
-  //     })
-  //     .catch((err) => console.log(err));
-  // }
-
-  // const amidDatabaseHandler = () => {
-  //   http
-  //     .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.amid}`)
-  //     .then((res) => {
-  //       console.log('amid' , res.data);
-  //       setResultData({...resultData, amid: res.data.data.results});
-  //     })
-  //     .catch((err) => console.log(err))
-  // }
-
-  // const moeinDatabaseHandler = () => {
-  //   http
-  //     .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.moein}`)
-  //     .then((res) => {
-  //       console.log('moein' , res.data);
-  //       setResultData({...resultData, moein: res.data.data.results})
-  //     })
-  //     .catch((err) => console.log(err))
-  // }
-
-  // const englishToPersianHandler = () => {
-  //   http
-  //     .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.en2fa}`)
-  //     .then((res) => {
-  //       setResultData({...resultData, english: res.data.data.results})
-  //     })
-  //     .catch((err) => console.log(err))
-  // }
-
-  // const suggestionsHandler = () => {
-  //   http
-  //     .get(`/suggest?token=${token}&q=${input}`)
-  //     .then((res) => {
-  //       setSuggestions(res.data.data.suggestion)
-  //     })
-  //     .catch((err) => { })
-  // }
-
-
-
-  // const showNewDatabases = () => {
-  //   setShowDatabases(!showDatabases)
-  // }
-
-  // const similarWordHandler = (e) => {
-  //   setInput(e.target.innerText)
-  //   dehkhodaDatabaseHandler()
-  //   amidDatabaseHandler()
-  //   moeinDatabaseHandler()
-  //   motaradefDatabaseHandler()
-  //   englishToPersianHandler()
-  //   suggestionsHandler()
-  // }
 
   return (
     <>
