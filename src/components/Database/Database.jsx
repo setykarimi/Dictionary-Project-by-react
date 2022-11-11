8d4c9d5dc09ec85a6beec4f95c541f43c3714d32
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-11 15:44:36 +0330

    clean code:)

--- src/components/Database/Database.jsx
+++ src/components/Database/Database.jsx
@@ -1,19 +1,19 @@
-const DatabaseBox = ({meaning , databseTitle}) => {
-    return (
-        <div className='meaning-section__box'  style={{display : meaning && meaning.length ? 'block' : 'none'}}>
-              {meaning && meaning.length !== 0 ?<div className='meaning-section__box-title'><span>لغت نامه: </span> <h3 className='database-title'>{databseTitle}</h3></div> : ''}
-              {meaning.map((item, key) => {
-                return (
-                    <div key={key}>
-                    <span className={item.pron ? 'pronunciation' : ''} style={{display : item.pron ? 'block' : 'none'}}>تلفظ {item.pron}</span>
-                    <p>
-                      {item.text}
-                    </p>
-                  </div>
-                )
-              })}
-            </div>
-    )
+const DatabaseBox = ({ meaning, databseTitle }) => {
+  return (
+    <div className='meaning-section__box' style={{ display: meaning && meaning.length ? 'block' : 'none' }}>
+      {meaning && meaning.length !== 0 ? <div className='meaning-section__box-title'><span>لغت نامه: </span> <h3 className='database-title'>{databseTitle}</h3></div> : ''}
+      {meaning.map((item, key) => {
+        return (
+          <div key={key}>
+            <span className={item.pron ? 'pronunciation' : ''} style={{ display: item.pron ? 'block' : 'none' }}>تلفظ {item.pron}</span>
+            <p>
+              {item.text}
+            </p>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default DatabaseBox
