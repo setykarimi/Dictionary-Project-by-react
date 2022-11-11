8cf3a67409936bb92456eeab3b6287f9e0b747d5
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-11 15:45:22 +0330

    comment all function except dehkhoda, remove useless states

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,11 +8,9 @@ import { http } from '../services/httpService';
 
 const HomePage = () => {
   const [input, setInput] = useState('');
-  const [database, setDatabase] = useState('dehkhoda');
   const [suggestions, setSuggestions] = useState([]);
   const databases =
-    { dehkhoda: "dehkhoda", amid: "amid", moein: "moein", motaradef: "motaradef", isfahani: "isfahani", en2fa: "en2fa" };
-  const [showDatabases, setShowDatabases] = useState(false);
+    { dehkhoda: "dehkhoda", amid: "amid", moein: "moein", motaradef: "motaradef", en2fa: "en2fa" };
   const [resultData, setResultData] = useState({
     dehkhoda: [],
     amid: [],
@@ -20,73 +18,65 @@ const HomePage = () => {
     motaradef: [],
     english: []
   });
+  const [loading, setLoading] = useState(false)
 
   const token = "68284.t5Gzego6SX28dzh71Un3aUZMAM2a0GIY7pkyzNEo";
 
-  useEffect(() => {
-    http
-      .get(`/search?token=${token}&q=${input}&type=exact&filter=${database}`)
-      .then((res) => {
-        dehkhodaDatabaseHandler();
-        amidDatabaseHandler()
-        moeinDatabaseHandler()
-        motaradefDatabaseHandler();
-        englishToPersianHandler()
-        // suggestionsHandler()
-        // showNewDatabases()
-      })
-      .catch((err) => console.log(err));
-  }, [input])
-
 
   const dehkhodaDatabaseHandler = () => {
+    setLoading(true)
     http
-      .get(`/search?token=${token}&q=${input}&type=exact&filter=${database}`)
+      .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.dehkhoda}`)
       .then((res) => {
-        setResultData({...resultData, dehkhoda: res.data.data.results});
+        console.log('dehkhoda', res.data.data.results);
+        setResultData({ ...resultData, dehkhoda: res.data.data.results });
+        setLoading(false);
       })
-      .catch((err) => console.log(err))
+      .catch((err) => {})
   }
 
-  const motaradefDatabaseHandler = () => {
-    http
-      .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.motaradef}`)
-      .then((res) => {
-        setResultData({...resultData, motaradef: res.data.data.results});
-      })
-      .catch((err) => console.log(err));
-  }
+  // const motaradefDatabaseHandler = () => {
+  //   http
+  //     .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.motaradef}`)
+  //     .then((res) => {
+  //       console.log('motaradef' , res.data);
+  //       setResultData({...resultData, motaradef: res.data.data.results});
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
 
-  const amidDatabaseHandler = () => {
-    http
-      .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.amid}`)
-      .then((res) => {
-        setResultData({...resultData, amid: res.data.data.results});
-      })
-      .catch((err) => console.log(err))
-  }
+  // const amidDatabaseHandler = () => {
+  //   http
+  //     .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.amid}`)
+  //     .then((res) => {
+  //       console.log('amid' , res.data);
+  //       setResultData({...resultData, amid: res.data.data.results});
+  //     })
+  //     .catch((err) => console.log(err))
+  // }
 
-  const moeinDatabaseHandler = () => {
-    http
-      .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.moein}`)
-      .then((res) => {
-        setResultData({...resultData, moein: res.data.data.results})
-      })
-      .catch((err) => console.log(err))
-  }
+  // const moeinDatabaseHandler = () => {
+  //   http
+  //     .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.moein}`)
+  //     .then((res) => {
+  //       console.log('moein' , res.data);
+  //       setResultData({...resultData, moein: res.data.data.results})
+  //     })
+  //     .catch((err) => console.log(err))
+  // }
 
-  const englishToPersianHandler = () => {
-    http
-      .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.en2fa}`)
-      .then((res) => {
-        setResultData({...resultData, english: res.data.data.results})
-      })
-      .catch((err) => console.log(err))
-  }
+  // const englishToPersianHandler = () => {
+  //   http
+  //     .get(`/search?token=${token}&q=${input}&type=exact&filter=${databases.en2fa}`)
+  //     .then((res) => {
+  //       setResultData({...resultData, english: res.data.data.results})
+  //     })
+  //     .catch((err) => console.log(err))
+  // }
 
   // const suggestionsHandler = () => {
-  //   axios
-  //     .get(`http://api.vajehyab.com/v3/suggest?token=${token}&q=${input}`)
+  //   http
+  //     .get(`/suggest?token=${token}&q=${input}`)
   //     .then((res) => {
   //       setSuggestions(res.data.data.suggestion)
   //     })
@@ -96,39 +86,39 @@ const HomePage = () => {
 
 
   const changeInputHandler = (e) => {
-    setInput(e.target.value)
+    setInput(e.target.value);
+    
   }
+
+
+
 
 
   const getResultHandler = (e) => {
     if (e.key === 'Enter') {
-      dehkhodaDatabaseHandler()
-      // amidDatabaseHandler()
-      // moeinDatabaseHandler()
-      // motaradefDatabaseHandler()
-      // suggestionsHandler()
-      // showNewDatabases()
-      // englishToPersianHandler()
+      dehkhodaDatabaseHandler();
     }
   }
 
-  const showNewDatabases = () => {
-    setShowDatabases(!showDatabases)
-  }
+  // const showNewDatabases = () => {
+  //   setShowDatabases(!showDatabases)
+  // }
 
-  const similarWordHandler = (e) => {
-    setInput(e.target.innerText)
-    dehkhodaDatabaseHandler()
-    // amidDatabaseHandler()
-    // moeinDatabaseHandler()
-    // motaradefDatabaseHandler()
-    // englishToPersianHandler()
-    // suggestionsHandler()
-  }
+  // const similarWordHandler = (e) => {
+  //   setInput(e.target.innerText)
+  //   dehkhodaDatabaseHandler()
+  //   amidDatabaseHandler()
+  //   moeinDatabaseHandler()
+  //   motaradefDatabaseHandler()
+  //   englishToPersianHandler()
+  //   suggestionsHandler()
+  // }
 
   return (
     <>
-      <Header input={input} changeInputHandler={changeInputHandler} getResultHandler={getResultHandler} />
+      <Header input={input} changeInputHandler={changeInputHandler}
+      getResultHandler={getResultHandler}
+      />
 
       <div className='container'>
         <Sidebar />
@@ -138,7 +128,7 @@ const HomePage = () => {
             <input placeholder='واژه مورد نظر خود را جستجو کنید' value={input} disabled />
           </div>
 
-          <div className='similar-words'>
+          {/* <div className='similar-words'>
             {suggestions.map((suggest, key) => {
               return (
                 <span key={key} onClick={similarWordHandler}>
@@ -146,14 +136,15 @@ const HomePage = () => {
                 </span>
               )
             })}
-          </div>
+          </div> */}
 
           <div className='meaning-section'>
+            {loading == true && <p style={{position: 'absolute', top: "50%", left: "50%"}}>loading</p>}
             <DatabaseBox meaning={resultData.dehkhoda} databseTitle='دهخدا' />
-            <DatabaseBox meaning={resultData.motaradef} databseTitle='مترادف و متضاد' />
+            {/* <DatabaseBox meaning={resultData.motaradef} databseTitle='مترادف و متضاد' />
             <DatabaseBox meaning={resultData.amid} databseTitle='عمید' />
             <DatabaseBox meaning={resultData.moein} databseTitle='معین' />
-            <DatabaseBox meaning={resultData.english} databseTitle='انگلیسی به فارسی' />
+            <DatabaseBox meaning={resultData.english} databseTitle='انگلیسی به فارسی' /> */}
           </div>
         </section>
       </div>
