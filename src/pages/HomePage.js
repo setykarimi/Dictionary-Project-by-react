b5dcf719c7cc1400ca2ff00d2221df8bc8c71a79
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-11 17:31:52 +0330

    remove content section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,14 +6,12 @@ const HomePage = ({ input, loading, resultData }) => {
 
   return (
     <>
+      <div className='search-term'>
+        <span>واژه جستجو شده: </span>
+        <input placeholder='واژه مورد نظر خود را جستجو کنید' value={input} disabled />
+      </div>
 
-      <section className='content'>
-        <div className='search-term'>
-          <span>واژه جستجو شده: </span>
-          <input placeholder='واژه مورد نظر خود را جستجو کنید' value={input} disabled />
-        </div>
-
-        {/* <div className='similar-words'>
+      {/* <div className='similar-words'>
             {suggestions.map((suggest, key) => {
               return (
                 <span key={key} onClick={similarWordHandler}>
@@ -23,18 +21,15 @@ const HomePage = ({ input, loading, resultData }) => {
             })}
           </div> */}
 
-        <div className='meaning-section'>
-          {loading == true && <p style={{ position: 'absolute', top: "50%", left: "50%" }}>loading</p>}
-          {resultData.dehkhoda.length !== 0 && <DatabaseBox meaning={resultData.dehkhoda} databseTitle='دهخدا' />}
-          {resultData.motaradef.length !== 0 && <DatabaseBox meaning={resultData.motaradef} databseTitle='مترادف و متضاد' />}
-          {resultData.amid.length !== 0 && <DatabaseBox meaning={resultData.amid} databseTitle='عمید' />}
-          {resultData.moein.length !== 0 && <DatabaseBox meaning={resultData.moein} databseTitle='معین' />}
-          {resultData.english.length !== 0 && <DatabaseBox meaning={resultData.english} databseTitle='انگلیسی به فارسی' />}
-        </div>
-      </section>
-
+      <div className='meaning-section'>
+        {loading == true && <p style={{ position: 'absolute', top: "50%", left: "50%" }}>loading</p>}
+        {resultData.dehkhoda.length !== 0 && <DatabaseBox meaning={resultData.dehkhoda} databseTitle='دهخدا' />}
+        {resultData.motaradef.length !== 0 && <DatabaseBox meaning={resultData.motaradef} databseTitle='مترادف و متضاد' />}
+        {resultData.amid.length !== 0 && <DatabaseBox meaning={resultData.amid} databseTitle='عمید' />}
+        {resultData.moein.length !== 0 && <DatabaseBox meaning={resultData.moein} databseTitle='معین' />}
+        {resultData.english.length !== 0 && <DatabaseBox meaning={resultData.english} databseTitle='انگلیسی به فارسی' />}
+      </div>
     </>
-
   );
 }
 
