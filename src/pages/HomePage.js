d685b8078fe59d7893451ab08054f8e377d83373
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-16 17:01:32 +0330

    Edit state names instead of resultData

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,9 @@ import '../components/Header/header.scss'
 import DatabaseBox from '../components/Database/Database';
 import '../styles/home.scss';
 
-const HomePage = ({ input, loading, resultData }) => {
+const HomePage = ({ input, loading, dehkhoda, amid, moein, motaradef, english, suggestions, similarWordHandler }) => {
+
+
 
   return (
     <>
@@ -11,23 +13,25 @@ const HomePage = ({ input, loading, resultData }) => {
         <input placeholder='واژه مورد نظر خود را جستجو کنید' value={input} disabled />
       </div>
 
-      {/* <div className='similar-words'>
-            {suggestions.map((suggest, key) => {
-              return (
-                <span key={key} onClick={similarWordHandler}>
-                  {suggest}
-                </span>
-              )
-            })}
-          </div> */}
+      {suggestions &&
+        <div className='similar-words'>
+          {suggestions.map((suggest, key) => {
+            return (
+              <span key={key} onClick={similarWordHandler}>
+                {suggest}
+              </span>
+            )
+          })}
+        </div>
+      }
 
       <div className='meaning-section'>
         {loading == true && <p style={{ position: 'absolute', top: "50%", left: "50%" }}>loading</p>}
-        {resultData.dehkhoda.length !== 0 && <DatabaseBox meaning={resultData.dehkhoda} databseTitle='دهخدا' />}
-        {resultData.motaradef.length !== 0 && <DatabaseBox meaning={resultData.motaradef} databseTitle='مترادف و متضاد' />}
-        {resultData.amid.length !== 0 && <DatabaseBox meaning={resultData.amid} databseTitle='عمید' />}
-        {resultData.moein.length !== 0 && <DatabaseBox meaning={resultData.moein} databseTitle='معین' />}
-        {resultData.english.length !== 0 && <DatabaseBox meaning={resultData.english} databseTitle='انگلیسی به فارسی' />}
+        {dehkhoda.length !== 0 && <DatabaseBox meaning={dehkhoda} databseTitle='دهخدا' />}
+        {motaradef.length !== 0 && <DatabaseBox meaning={motaradef} databseTitle='مترادف و متضاد' />}
+        {amid.length !== 0 && <DatabaseBox meaning={amid} databseTitle='عمید' />}
+        {moein.length !== 0 && <DatabaseBox meaning={moein} databseTitle='معین' />}
+        {english.length !== 0 && <DatabaseBox meaning={english} databseTitle='انگلیسی به فارسی' />}
       </div>
     </>
   );
